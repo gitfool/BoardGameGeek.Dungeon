80b130adbb47dc0212ff54495d41e87921b7d63a
Sean Fausett <sean@jellyfish.co.nz>
2020-01-20 18:29:36 +1300

    Bump System.CommandLine to 0.3.0-alpha.20054.1

--- Application/CommandLine.cs
+++ Application/CommandLine.cs
@@ -10,16 +10,26 @@ namespace BoardGameGeek.Dungeon
     {
         static CommandLine()
         {
-            var allOption = new Option(new[] { "-a", "--all" }, "Analyze all override.", new Argument<bool>());
-            var yearOption = new Option(new[] { "-y", "--year" }, "Year to analyze. Defaults to current year.", new Argument<int?>(DateTime.Now.Year));
+            var userArgument = new Argument<string> { Name = "userName", Description = "Geek user name." };
 
-            Parser = new CommandLineBuilder(new RootCommand("A command line tool for interacting with the BoardGameGeek API")
-            {
-                new Command("plays", "Get user plays.", new[] { allOption, yearOption }, new Argument<string> { Name = "userName", Description = "Geek user name." },
-                    handler: CommandHandler.Create<string, bool, int?>(PlaysAsync)),
-                new Command("stats", "Get user stats.", new[] { allOption, yearOption }, new Argument<string> { Name = "userName", Description = "Geek user name." },
-                    handler: CommandHandler.Create<string, bool, int?>(StatsAsync))
-            })
+            var allOption = new Option(new[] { "-a", "--all" }, "Analyze all override.") { Argument = new Argument<bool>() };
+            var yearOption = new Option(new[] { "-y", "--year" }, "Year to analyze. Defaults to current year.") { Argument = new Argument<int?>(() => DateTime.Now.Year) };
+
+            var playsCommand = new CommandBuilder(new Command("plays", "Get user plays.") { Handler = CommandHandler.Create<string, bool, int?>(PlaysAsync) })
+                .AddArgument(userArgument)
+                .AddOption(allOption)
+                .AddOption(yearOption)
+                .Command;
+
+            var statsCommand = new CommandBuilder(new Command("stats", "Get user stats.") { Handler = CommandHandler.Create<string, bool, int?>(StatsAsync) })
+                .AddArgument(userArgument)
+                .AddOption(allOption)
+                .AddOption(yearOption)
+                .Command;
+
+            Parser = new CommandLineBuilder(new RootCommand("A command line tool for interacting with the BoardGameGeek API"))
+                .AddCommand(playsCommand)
+                .AddCommand(statsCommand)
                 .UseDefaults()
                 .Build();
         }
