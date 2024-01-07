29e86d9285e3519bad8b35ef01470fad8e0bd12b
Sean Fausett <sean@jellyfish.co.nz>
2024-01-07 20:48:27 +1300

    Fix model tests

--- Tests/ModelTests.cs
+++ Tests/ModelTests.cs
@@ -16,7 +16,7 @@ public sealed class ModelTests
         [InlineData("Highlights: 2014, 2018.")]
         [InlineData(@"Highlight 2014\nHighlight 2018")]
         [InlineData(@"Highlight: 2014.\nHighlight: 2018.")]
-        public void Should_ReturnFalse_WhenHighlightYear_IsNotInComments(string comments)
+        public void Should_ReturnFalse_WhenHighlightYear_IsNotInComments(string? comments)
         {
             var isHighlight = Game.IsHighlightMatch(comments, 2016);
 
@@ -32,7 +32,7 @@ public sealed class ModelTests
         [InlineData(@"Highlight: 2014.\nHighlight: 2016.")]
         [InlineData(@"Highlights 2010 2012\nHighlights 2014 2016 2018")]
         [InlineData(@"Highlights: 2010, 2012.\nHighlights: 2014, 2016, 2018.")]
-        public void Should_ReturnTrue_WhenHighlightYear_IsInComments(string comments)
+        public void Should_ReturnTrue_WhenHighlightYear_IsInComments(string? comments)
         {
             var isHighlight = Game.IsHighlightMatch(comments, 2016);
 
@@ -50,7 +50,7 @@ public sealed class ModelTests
         [InlineData("Session.")]
         [InlineData("Session ?/?")]
         [InlineData("Session: ?/?.")]
-        public void Should_ReturnFalse_WhenSessionStats_AreNotInComments(string comments)
+        public void Should_ReturnFalse_WhenSessionStats_AreNotInComments(string? comments)
         {
             var session = Play.IsSessionMatch(comments);
 
@@ -64,7 +64,7 @@ public sealed class ModelTests
         [InlineData("Session: 1/2.")]
         [InlineData("Session 2/2")]
         [InlineData("Session: 2/2.")]
-        public void Should_ReturnTrue_WhenSessionStats_AreInComments(string comments)
+        public void Should_ReturnTrue_WhenSessionStats_AreInComments(string? comments)
         {
             var session = Play.IsSessionMatch(comments);
 
